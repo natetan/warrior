71c7ac1839a2fed10bfdbce28fbd6fdcf08d1416
Yulong Tan <yulongtan67@gmail.com>
2019-04-02 21:34:59 -0700

    Add RestClient helper

---
+++ client/RestClient.js
@@ -0,0 +1,30 @@
+let fetch = require('node-fetch');
+let fs = require('fs');
+
+async function GetAsync(url, options = {}) {
+  try {
+    let res = await fetch(url, options);
+    return res.json();
+  } catch (err) {
+    console.log(`Error in <RestClient.GetAsync()>: ${err}`);
+    return null;
+  }
+}
+
+async function SaveJsonAsync(url, options = {}) {
+  try {
+    let json = await GetAsync(url, options);
+    fs.writeFileSync('results.json', JSON.stringify(json, null, 2), (err) => {
+      console.log(`File save error: ${err}`);
+      return true;
+    });
+  } catch (e) {
+    console.log(`Error in <RestClient.SaveJsonAsync()>: ${err}`);
+    return false;
+  }
+}
+
+module.exports = {
+  GetAsync: GetAsync,
+  SaveJsonAsync: SaveJsonAsync
+}
