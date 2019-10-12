888ff556911ca76a370befa0747366f1af74723f
Yulong Tan <yulongtan67@gmail.com>
2019-10-11 18:07:09 -0700

    Add initial delete

---
+++ commands/delete.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const discordUtils = require('../utils/discordUtils');
+const Jimp = require('jimp');
+
+module.exports = {
+  name: 'delete',
+  desc: 'delet.',
+  commandType: 'special',
+  category: 'imgen',
+  async execute(message, args, client) {
+    let m = await message.channel.send('Processing imgen...');
+    try {
+      const avatars = discordUtils.getAvatars(message, client);
+      let target = await Jimp.read(avatars.target);
+      let base = await Jimp.read('https://raw.githubusercontent.com/fu-snail/Arcane-Vortex/master/resources/images/memes/delete.bmp');
+      let outputName = 'delete.png';
+
+      target.resize(195, 195);
+      // const offset = 30;
+      // base.crop(offset, 0, base.getWidth() - offset, base.getHeight());
+      // await target.blur(6);
+      await base.composite(target, 120, 135);
+      let error, res = await base.getBufferAsync(Jimp.MIME_PNG);
+      const attachment = new Discord.Attachment(res, outputName);
+      await message.channel.send('', attachment);
+      return m.delete();
+    } catch (err) {
+      console.log(`ERROR: Command <delete> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      await m.edit('Sorry, there was an error.');
+    }
+  }
+}
