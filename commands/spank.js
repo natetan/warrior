5547cc32320a575024de0fe948b61062ede09339
Yulong Tan <yulongtan67@gmail.com>
2019-10-14 16:45:52 -0700

    Add initial bed command

--- commands/spank.js
+++ commands/spank.js
@@ -3,8 +3,8 @@ const discordUtils = require('../utils/discordUtils');
 const Jimp = require('jimp');
 
 module.exports = {
-  name: 'spank',
-  desc: 'Spanks someone.',
+  name: 'bed',
+  desc: 'Brothers are mean.',
   commandType: 'special',
   category: 'imgen',
   async execute(message, args, client) {
@@ -12,28 +12,32 @@ module.exports = {
     try {
       const avatars = discordUtils.getAvatars(message, client);
 
-      let spanker = await Jimp.read(avatars.self);
-      let spankee = await Jimp.read(avatars.target);
-      let base = await Jimp.read('https://raw.githubusercontent.com/fu-snail/Arcane-Vortex/master/resources/images/memes/spank.bmp');
+      let avatar1 = await Jimp.read(avatars.self);
+      let avatar2 = await Jimp.read(avatars.target);
+      let avatarCopy = await Jimp.read(avatars.self);
+      let base = await Jimp.read('https://raw.githubusercontent.com/fu-snail/Arcane-Vortex/master/resources/images/memes/bed.bmp');
       let outputName = 'spank.png';
 
-      spanker.resize(140, 140);
-      spankee.resize(120, 120);
+      avatar1.resize(100, 100);
+      avatar2.resize(70, 70);
+      avatarCopy.resize(70, 70);
 
-      const offset = 30;
-      base.crop(offset, 0, base.getWidth() - offset, base.getHeight());
+      // const offset = 30;
+      // base.crop(offset, 0, base.getWidth() - offset, base.getHeight());
 
       await base
         .resize(500, 500)
-        .composite(spanker, 225, 2.5)
-        .composite(spankee, 370, 220);
+        .composite(avatar1, 25, 100)
+        .composite(avatar1, 25, 300)
+        .composite(avatarCopy, 53, 450)
+        .composite(avatar2, 53, 575);
 
       let error, res = await base.getBufferAsync(Jimp.MIME_PNG);
       const attachment = new Discord.Attachment(res, outputName);
       await message.channel.send('', attachment);
       return m.delete();
     } catch (err) {
-      console.log(`ERROR: Command <spank> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      console.log(`ERROR: Command <bed> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
       await m.edit('Sorry, there was an error.');
     }
   }
