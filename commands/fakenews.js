0c8cfa992f9ac7a3338bdcb95f5ba6d4a33b6827
Yulong Tan <yulongtan67@gmail.com>
2019-10-16 16:15:48 -0700

    Complete jail and add initial fakenews

--- commands/fakenews.js
+++ commands/fakenews.js
@@ -3,36 +3,36 @@ const discordUtils = require('../utils/discordUtils');
 const Jimp = require('jimp');
 
 module.exports = {
-  name: 'jail',
-  desc: 'Them them to jail!',
+  name: 'fakenews',
+  desc: 'FOX is not the only fake news channel.',
   commandType: 'special',
   category: 'imgen',
   async execute(message, args, client) {
     let m = await message.channel.send('Processing imgen...');
     try {
       const avatars = discordUtils.getAvatars(message, client);
-      const imageURL = 'https://raw.githubusercontent.com/fu-snail/Arcane-Vortex/master/resources/images/memes/jail.bmp';
+      const imageURL = 'https://raw.githubusercontent.com/fu-snail/Arcane-Vortex/master/resources/images/memes/fakenews.bmp';
       let avatar = await Jimp.read(avatars.target);
       let base = await Jimp.read(imageURL);
-      let outputName = 'jail.png';
+      let baseFinal = await Jimp.read(base);
+      let outputName = 'fakenews.png';
 
-      avatar.resize(350, 350);
-      base.resize(350, 350).rgba(true);
+      avatar.resize(400, 400);
 
-      await avatar
+      await baseFinal
+        .composite(avatar, 390, 0)
         .composite(base, 0, 0, {
           mode: Jimp.BLEND_SOURCE_OVER,
           opacityDest: 1,
           opacitySource: 0.5
-        })
-        .grayscale();
+        });
 
-      let error, res = await avatar.getBufferAsync(Jimp.MIME_PNG);
+      let error, res = await baseFinal.getBufferAsync(Jimp.MIME_PNG);
       const attachment = new Discord.Attachment(res, outputName);
       await message.channel.send('', attachment);
       return m.delete();
     } catch (err) {
-      console.log(`ERROR: Command <jail> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      console.log(`ERROR: Command <fakenews> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
       await m.edit('Sorry, there was an error.');
     }
   }
