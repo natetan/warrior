e7c7b452f55242cd0e1e1757acb6a17d3fa98c10
Yulong Tan <yulongtan67@gmail.com>
2019-10-16 14:14:12 -0700

    Remove fakenews and add initial failure

--- commands/failure.js
+++ commands/failure.js
@@ -3,8 +3,8 @@ const discordUtils = require('../utils/discordUtils');
 const Jimp = require('jimp');
 
 module.exports = {
-  name: 'fakenews',
-  desc: 'Some people get fooled by the news, but not us.',
+  name: 'failure',
+  desc: 'Who is a failure?',
   commandType: 'special',
   category: 'imgen',
   async execute(message, args, client) {
@@ -14,27 +14,22 @@ module.exports = {
       const imageURL = 'https://raw.githubusercontent.com/fu-snail/Arcane-Vortex/master/resources/images/memes/fakenews.bmp';
       let avatar = await Jimp.read(avatars.target);
       let base = await Jimp.read(imageURL);
-      let finalBase = await Jimp.read(imageURL);
       let outputName = 'fakenews.png';
 
-      avatar.resize(400, 400);
+      avatar.resize(215, 215);
 
       // const offset = 30;
       // base.crop(offset, 0, base.getWidth() - offset, base.getHeight());
 
-      // await base
-      //   .resize(500, 500)
-      //   .composite(avatar, 370, 220);
-      await finalBase
-        .composite(avatar, 390, 0)
-        .composite(base, 0, 0);
+      await base
+        .composite(avatar, 143, 525);
 
-      let error, res = await finalBase.getBufferAsync(Jimp.MIME_PNG);
+      let error, res = await base.getBufferAsync(Jimp.MIME_PNG);
       const attachment = new Discord.Attachment(res, outputName);
       await message.channel.send('', attachment);
       return m.delete();
     } catch (err) {
-      console.log(`ERROR: Command <fakenews> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      console.log(`ERROR: Command <failure> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
       await m.edit('Sorry, there was an error.');
     }
   }
