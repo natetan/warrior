cd53ef31efb3490e747aec67792ba373d7fba98f
Yulong Tan <yulongtan67@gmail.com>
2019-10-22 13:57:27 -0700

    Add invert and fix egg

--- commands/invert.js
+++ commands/invert.js
@@ -3,29 +3,26 @@ const discordUtils = require('../utils/discordUtils');
 const Jimp = require('jimp');
 
 module.exports = {
-  name: 'egg',
-  desc: 'Duck pooping out an egg',
+  name: 'invert',
+  desc: 'inverts someone.',
   commandType: 'special',
   category: 'imgen',
   async execute(message, args, client) {
     let m = await message.channel.send('Processing imgen...');
     try {
       const avatars = discordUtils.getAvatars(message, client);
-      let egg = await Jimp.read(avatars.target);
-      let base = await Jimp.read('https://raw.githubusercontent.com/fu-snail/Arcane-Vortex/master/resources/images/memes/egg.bmp');
-      let outputName = 'egg.png';
 
-      egg.resize(50, 50);
-      await base
-        .resize(350, 350)
-        .composite(egg, 143, 188);
-      
-      let error, res = await base.getBufferAsync(Jimp.MIME_PNG);
+      let avatar = await Jimp.read(avatars.target);
+      let outputName = 'invert.png';
+
+      avatar.resize(400, 400).invert();
+
+      let error, res = await avatar.getBufferAsync(Jimp.MIME_PNG);
       const attachment = new Discord.Attachment(res, outputName);
       await message.channel.send('', attachment);
       return m.delete();
     } catch (err) {
-      console.log(`ERROR: Command <egg> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      console.log(`ERROR: Command <invert> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
       await m.edit('Sorry, there was an error.');
     }
   }
