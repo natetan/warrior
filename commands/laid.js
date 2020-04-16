480acb6e17726f91703a712848041a465ec2ca63
Nate Tan <nytan67@gmail.com>
2020-04-16 02:04:19 -0700

    Add laid command

---
+++ commands/laid.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const discordUtils = require('../utils/discordUtils');
+const Jimp = require('jimp');
+
+module.exports = {
+  name: 'laid',
+  desc: 'The kind of person who does not get laid.',
+  commandType: 'special',
+  category: 'imgen',
+  async execute(message, args, client) {
+    let m = await message.channel.send('Processing imgen...');
+    try {
+      const avatars = discordUtils.getAvatars(message, client);
+      let target = await Jimp.read(avatars.target);
+      let base = await Jimp.read('https://raw.githubusercontent.com/natetan/warrior/master/resources/images/memes/laid.png');
+      let circleMask = await Jimp.read('https://raw.githubusercontent.com/natetan/warrior/master/resources/images/memes/circle-mask.png');
+      let outputName = 'laid.png';
+
+      // Put circle over image
+      target.resize(115, 115);
+      circleMask.resize(115 ,115);
+      target.mask(circleMask, 0, 0);
+
+      await base.composite(target, 512, 360);
+      let error, res = await base.getBufferAsync(Jimp.MIME_PNG);
+      const attachment = new Discord.Attachment(res, outputName);
+      await message.channel.send('', attachment);
+      return m.delete();
+    } catch (err) {
+      console.log(`ERROR: Command <laid> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      await m.edit('Sorry, there was an error.');
+    }
+  }
+}
