ed6c126d632dd921198cf6235c2b19a650652887
Nate Tan <nytan67@gmail.com>
2020-04-22 15:52:38 -0700

    Add chidori meme

---
+++ commands/chidori.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const discordUtils = require('../utils/discordUtils');
+const Jimp = require('jimp');
+
+module.exports = {
+  name: 'chidori',
+  desc: 'When the beat hits you like a chidori to the heart',
+  commandType: 'special',
+  category: 'imgen',
+  async execute(message, args, client) {
+    let m = await message.channel.send('Processing imgen...');
+    try {
+      const avatars = discordUtils.getAvatars(message, client);
+
+      let kakashi = await Jimp.read(avatars.self);
+      let rin = await Jimp.read(avatars.target);
+      let base = await Jimp.read('https://raw.githubusercontent.com/natetan/warrior/master/resources/images/memes/chidori.jpg');
+      let outputName = 'chidori.jpg';
+
+      rin.resize(215, 215);
+      kakashi.resize(225, 225);
+      await base
+        .composite(rin, 490, 45)
+        .composite(kakashi, 730, 150)
+
+      let error, res = await base.getBufferAsync(Jimp.MIME_PNG);
+      const attachment = new Discord.Attachment(res, outputName);
+      await message.channel.send('', attachment);
+      return m.delete();
+    } catch (err) {
+      console.log(`ERROR: Command <chidori> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      await m.edit('Sorry, there was an error.');
+    }
+  }
+}
