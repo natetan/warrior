be3bb40b11d2e8a599af277f6470adde63259b1b
Nate Tan <nytan67@gmail.com>
2020-07-14 00:24:25 -0700

    Add quotes freom vAS

--- commands/llothis.js
+++ commands/llothis.js
@@ -1,20 +1,19 @@
-const emojis = require('../resources/emojis');
 const quotes = require('../resources/quotes.json');
 const destroy = require('../resources/destroy.json');
 const quoteUtils = require('../utils/quoteUtils');
 
 module.exports = {
-  name: 'warrior',
-  desc: 'Warrior quotes from ESO.',
+  name: 'llothis',
+  desc: 'Saint Llothis the Pious quotes from ESO.',
   commandType: 'special',
   category: 'quotes',
   async execute(message, args, client) {
     try {
-      let m = quoteUtils.determineQuote(message, quotes.warrior, destroy.warrior, client.emojis.cache.get(emojis.customEmojis.warrior));
+      let m = quoteUtils.determineQuote(message, quotes.llothis, destroy.llothis);
       return message.channel.send(m);
 
     } catch (err) {
-      console.log(`ERROR: Command <warrior> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      console.log(`ERROR: Command <llothis> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
     }
   }
 }
