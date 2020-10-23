381d0c82461d1d35f48a5db74d45076c257e1610
Nate Tan <nytan67@gmail.com>
2020-10-23 15:20:07 -0700

    Rename airpods to draw

--- commands/draw.js
+++ commands/draw.js
@@ -3,17 +3,17 @@ const discordUtils = require('../utils/discordUtils');
 const Jimp = require('jimp');
 
 module.exports = {
-  name: 'airpods',
-  desc: 'Old man with airpods and iPad.',
+  name: 'draw',
+  desc: 'Draw me like one of your french girls.',
   commandType: 'special',
   category: 'imgen',
   async execute(message, args, client) {
     let m = await message.channel.send('Processing imgen...');
     try {
       const avatars = discordUtils.getAvatars(message, client);
-      let person = await Jimp.read(avatars.target);
-      let base = await Jimp.read('https://raw.githubusercontent.com/natetan/warrior/master/resources/images/memes/airpods.png');
-      let outputName = 'airpods.png';
+      const person = await Jimp.read(avatars.target);
+      const base = await Jimp.read('https://raw.githubusercontent.com/natetan/warrior/master/resources/images/memes/draw.png');
+      const outputName = 'draw.png';
 
       person.resize(325, 500).rotate(21.6);
       await base.composite(person, 480, 365)
@@ -22,7 +22,7 @@ module.exports = {
       await message.channel.send('', attachment);
       return m.delete();
     } catch (err) {
-      console.log(`ERROR: Command <airpods> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      console.log(`ERROR: Command <draw> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
       await m.edit('Sorry, there was an error.');
     }
   }
