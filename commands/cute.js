9167ac5d2dd3c6e94a9f18c230d15a380f015cd5
Nate Tan <nytan67@gmail.com>
2020-10-23 13:35:10 -0700

    Add cute imgen

---
+++ commands/cute.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const discordUtils = require('../utils/discordUtils');
+const Jimp = require('jimp');
+
+module.exports = {
+  name: 'cute',
+  desc: 'Someone is looking cute',
+  commandType: 'special',
+  category: 'imgen',
+  async execute(message, args, client) {
+    let m = await message.channel.send('Processing imgen...');
+    try {
+      const avatars = discordUtils.getAvatars(message, client);
+      let person = await Jimp.read(avatars.target);
+      let base = await Jimp.read('https://raw.githubusercontent.com/natetan/warrior/master/resources/images/memes/cute.png');
+      let outputName = 'cute.png';
+
+      person.resize(500, 500);
+      await base.composite(person, 675, 220);
+      let error, res = await base.getBufferAsync(Jimp.MIME_PNG);
+      const attachment = new Discord.MessageAttachment(res, outputName);
+      await message.channel.send('', attachment);
+      return m.delete();
+    } catch (err) {
+      console.log(`ERROR: Command <cute> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      await m.edit('Sorry, there was an error.');
+    }
+  }
+}
