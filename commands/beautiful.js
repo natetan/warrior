7f8bccf5d5ec794838e7d975f402fc2634177b5b
Nate Tan <nytan67@gmail.com>
2020-10-23 14:30:16 -0700

    add beautiful

---
+++ commands/beautiful.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const discordUtils = require('../utils/discordUtils');
+const Jimp = require('jimp');
+
+module.exports = {
+  name: 'beautiful',
+  desc: 'Someone is looking beautiful',
+  commandType: 'special',
+  category: 'imgen',
+  async execute(message, args, client) {
+    let m = await message.channel.send('Processing imgen...');
+    try {
+      const avatars = discordUtils.getAvatars(message, client);
+      let person = await Jimp.read(avatars.target);
+      let base = await Jimp.read('https://raw.githubusercontent.com/natetan/warrior/master/resources/images/memes/beautiful.png');
+      let outputName = 'beautiful.png';
+
+      person.resize(150, 150);
+      await base.composite(person, 333, 30).composite(person, 350, 395);
+      let error, res = await base.getBufferAsync(Jimp.MIME_PNG);
+      const attachment = new Discord.MessageAttachment(res, outputName);
+      await message.channel.send('', attachment);
+      return m.delete();
+    } catch (err) {
+      console.log(`ERROR: Command <beautiful> failed.\n\tMessage: [${message}]\n\tError: [${err}]`);
+      await m.edit('Sorry, there was an error.');
+    }
+  }
+}
