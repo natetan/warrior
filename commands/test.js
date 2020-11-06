bd694d60b7adb0cbd99bdbd34dc3b0eeac5429a6
Nate Tan <nytan67@gmail.com>
2020-11-06 13:18:22 -0800

    Change testing command

--- commands/test.js
+++ commands/test.js
@@ -6,13 +6,6 @@ module.exports = {
   desc: 'Testing shit goes here.',
   commandType: 'private',
   async execute(message, args, client) {
-    const num = Number(args[0]);
-    if (!num) {
-      return message.channel.send('That is not a number');
-    }
-    for (let i = 0; i < num; i++) {
-      message.channel.send(i + 1);
-    }
-    return;
+    client.emit('guildMemberAdd', message.member);
   }
 }
