1b478de943c9a905c24046df8efc12c211206a5d
Nate Tan <nytan67@gmail.com>
2020-11-06 16:46:39 -0800

    emit back for test

--- commands/test.js
+++ commands/test.js
@@ -1,11 +1,13 @@
 const eu = require('../utils/embedUtils');
 const emojis = require('../resources/emojis');
 
+const guildHelper = require('../database/guild');
+
 module.exports = {
   name: 'test',
   desc: 'Testing shit goes here.',
   commandType: 'private',
   async execute(message, args, client) {
-    client.emit('guildMemberAdd', message.member);
+    client.emit('guildMemberAdd', message.member)
   }
 }
